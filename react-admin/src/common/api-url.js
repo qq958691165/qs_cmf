5c9fadaed0739a7eb1d5a86ef207a367559dddd2
jack <958691165@qq.com>
2024-11-07 10:02:59 +0800

    删除react-admin，后面采用vite编译

--- react-admin/src/common/api-url.js
+++
@@ -1,8 +0,0 @@
-const ApiUrl = {
-    reactBuildTestGet: '/admin/ReactBuildTest/getTest',
-    reactBuildTestPost: '/admin/ReactBuildTest/createTest',
-    reactBuildTestPut: '/admin/ReactBuildTest/updateTest',
-    reactBuildTestDelete: '/admin/ReactBuildTest/deleteTest',
-}
-
-export default ApiUrl;
